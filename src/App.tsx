c979661abdd4a77482a3c7e2be738e546205e329
Dan Philibin <dan@danphilibin.com>
2023-06-17 20:04:15 +0000

    add scrim to the top

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,17 @@ function App() {
 
   return (
     <>
-      <div className="h-8" data-tauri-drag-region></div>
-      {initialContent !== null && (
-        <div className="p-3 pt-0">
-          <Editor initialContent={initialContent} />
-        </div>
-      )}
+      <div
+        className="h-10 bg-gradient-to-b from-white to-white/0  z-10 absolute top-0 left-0 w-full"
+        data-tauri-drag-region
+      ></div>
+      <div className="h-screen overflow-scroll">
+        {initialContent !== null && (
+          <div className="p-3 pt-6">
+            <Editor initialContent={initialContent} />
+          </div>
+        )}
+      </div>
     </>
   );
 }
